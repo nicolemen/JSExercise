3a898cf41594281d568c5187ea0a896c1af194ad
nicolemen <nicole.men.21@bishops.com>
2021-01-12 14:42:44 -0800

    finished assignment

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // function after clicking button
-function clickButton(){
+function clickButton() {
   // get values from user input
   var name = document.getElementById('name').value;
+  var phone = document.getElementById('phone').value;
+  var bdate = document.getElementById('bdate').value;
   var red = document.getElementById('red');
   var orange = document.getElementById('orange');
   var yellow = document.getElementById('yellow');
@@ -9,46 +11,47 @@ function clickButton(){
   var blue = document.getElementById('blue');
   var purple = document.getElementById('purple');
 
-  // the following radio button-related stuff doesn't actually work because they can't access the specific user inputs
-  //var age = document.getElementById('age').value;
+  // replaces questions with a confirmation
   jsItems.innerHTML = "\n";
   jsItems.innerHTML += "\t\t<h1>Thanks for submitting, " + name + "!</h1>\n";
-  //jsItems.innerHTML += "\t\t<p>You are " + age + ", and your favorite color(s) is/are " + color + ".</p>\n";
-  if (red.checked){
+
+  jsItems.innerHTML += "\t\t<p>Your phone number is " + phone + ".</p>\n";
+  jsItems.innerHTML += "\t\t<p>Your birthday is " + bdate + ".</p>\n";
+
+  if (red.checked) {
     jsItems.innerHTML += "\t\t<p>Your favorite color is red.</p>\n";
-  }
-  else{
+  } else {
     jsItems.innerHTML += "\t\t<p>You don't like red.</p>\n";
   }
-  if (orange.checked){
+  if (orange.checked) {
     jsItems.innerHTML += "\t\t<p>Your favorite color is orange.</p>\n";
-  }
-  else{
+  } else {
     jsItems.innerHTML += "\t\t<p>You don't like orange.</p>\n";
   }
-  if (yellow.checked){
+  if (yellow.checked) {
     jsItems.innerHTML += "\t\t<p>Your favorite color is yellow.</p>\n";
-  }
-  else{
+  } else {
     jsItems.innerHTML += "\t\t<p>You don't like yellow.</p>\n";
   }
-  if (green.checked){
+  if (green.checked) {
     jsItems.innerHTML += "\t\t<p>Your favorite color is green.</p>\n";
-  }
-  else{
+  } else {
     jsItems.innerHTML += "\t\t<p>You don't like green.</p>\n";
   }
-  if (blue.checked){
+  if (blue.checked) {
     jsItems.innerHTML += "\t\t<p>Your favorite color is blue.</p>\n";
-  }
-  else{
+  } else {
     jsItems.innerHTML += "\t\t<p>You don't like blue.</p>\n";
   }
-  if (purple.checked){
+  if (purple.checked) {
     jsItems.innerHTML += "\t\t<p>Your favorite color is purple.</p>\n";
-  }
-  else{
+  } else {
     jsItems.innerHTML += "\t\t<p>You don't like purple.</p>\n";
   }
+
+  // stores and returns the name of the last user
+  var lastUser = localStorage.getItem("name");
+  jsItems.innerHTML += "\t\t<p>The last user was " + lastUser + ".</p>\n";
+  localStorage.setItem("name", name);
 
 }
