434ee762bbbf3e5f43c485c15cfb147ff0be15da
nicolemen <nicole.men.21@bishops.com>
2021-01-13 09:36:09 -0800

    added JSON

--- script.js
+++ script.js
@@ -11,39 +11,51 @@ function clickButton() {
   var blue = document.getElementById('blue');
   var purple = document.getElementById('purple');
 
+  myJSON = {
+    'name': name,
+    'phone': phone,
+    'bdate': bdate,
+    'red': red.checked,
+    'orange': orange.checked,
+    'yellow': yellow.checked,
+    'green': green.checked,
+    'blue': blue.checked,
+    'purple': purple.checked,
+  }
+
   // replaces questions with a confirmation
   jsItems.innerHTML = "\n";
-  jsItems.innerHTML += "\t\t<h1>Thanks for submitting, " + name + "!</h1>\n";
+  jsItems.innerHTML += "\t\t<h1>Thanks for submitting, " + myJSON["name"] + "!</h1>\n";
 
-  jsItems.innerHTML += "\t\t<p>Your phone number is " + phone + ".</p>\n";
-  jsItems.innerHTML += "\t\t<p>Your birthday is " + bdate + ".</p>\n";
+  jsItems.innerHTML += "\t\t<p>Your phone number is " + myJSON["phone"] + ".</p>\n";
+  jsItems.innerHTML += "\t\t<p>Your birthday is " + myJSON["bdate"] + ".</p>\n";
 
-  if (red.checked) {
+  if (myJSON["red"]) {
     jsItems.innerHTML += "\t\t<p>Your favorite color is red.</p>\n";
   } else {
     jsItems.innerHTML += "\t\t<p>You don't like red.</p>\n";
   }
-  if (orange.checked) {
+  if (myJSON["orange"]) {
     jsItems.innerHTML += "\t\t<p>Your favorite color is orange.</p>\n";
   } else {
     jsItems.innerHTML += "\t\t<p>You don't like orange.</p>\n";
   }
-  if (yellow.checked) {
+  if (myJSON["yellow"]) {
     jsItems.innerHTML += "\t\t<p>Your favorite color is yellow.</p>\n";
   } else {
     jsItems.innerHTML += "\t\t<p>You don't like yellow.</p>\n";
   }
-  if (green.checked) {
+  if (myJSON["green"]) {
     jsItems.innerHTML += "\t\t<p>Your favorite color is green.</p>\n";
   } else {
     jsItems.innerHTML += "\t\t<p>You don't like green.</p>\n";
   }
-  if (blue.checked) {
+  if (myJSON["blue"]) {
     jsItems.innerHTML += "\t\t<p>Your favorite color is blue.</p>\n";
   } else {
     jsItems.innerHTML += "\t\t<p>You don't like blue.</p>\n";
   }
-  if (purple.checked) {
+  if (myJSON["purple"]) {
     jsItems.innerHTML += "\t\t<p>Your favorite color is purple.</p>\n";
   } else {
     jsItems.innerHTML += "\t\t<p>You don't like purple.</p>\n";
@@ -52,6 +64,6 @@ function clickButton() {
   // stores and returns the name of the last user
   var lastUser = localStorage.getItem("name");
   jsItems.innerHTML += "\t\t<p>The last user was " + lastUser + ".</p>\n";
-  localStorage.setItem("name", name);
+  localStorage.setItem("name", myJSON["name"]);
 
 }
